d7ba747c5dddcedbdea0aeba880af862f9c389f5
AnnNavarrete <ann.g.navarrete@gmail.com>
2019-06-11 17:28:28 +1000

    adding a pie chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Chart from "./Chart";
-import { Pie } from "react-chartjs-2";
 
 function App() {
   return (
--- frontend/src/Chart.js
+++ frontend/src/Chart.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Pie } from "react-chartjs-2";
 
 export default class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        datasets: [
+          {
+            data: [10, 20, 30, 40, 5]
+          }
+        ],
+        labels: ["House", "Food", "Utilities", "Entertainment", "Transport"],
+        backgroundColor: ["255,0,0"]
+      }
+    };
+  }
+
   render() {
-    return <div>Testing Chart</div>;
+    return (
+      <div className="chart">
+        <Pie
+          data={this.state.data}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+      </div>
+    );
   }
 }
